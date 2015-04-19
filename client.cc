47c7f91ba8069b307b9fa02eeaa3779fc31baf38
bikli <460720965@qq.com>
2015-04-19 15:43:56 +0800

    simple chat

--- client.cc
+++ client.cc
@@ -6,11 +6,13 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <unistd.h>
-
+#include <sys/epoll.h>
 int main(int argc , char * argv[])
 {
 	struct sockaddr_in clientaddr;
-	int clientfd,port;
+	struct epoll_event env , events[4];
+	int clientfd,port,epfd;
+	int stdno=fileno(stdin);
 	socklen_t client_len;	
 	char buf[255];
 	if(argc<3)
@@ -35,9 +37,38 @@ int main(int argc , char * argv[])
 		printf("connect error %d\n",errno);
 		return 0;
 	}
-	printf("you: ");
-	scanf("%s",buf);
-	write(clientfd,buf,255);
+	epfd = epoll_create(5);
+	env.data.fd = clientfd;
+	env.events = EPOLLIN;
+	epoll_ctl(epfd , EPOLL_CTL_ADD , clientfd , &env);
+	env.data.fd = stdno;
+	env.events = EPOLLIN;
+	epoll_ctl(epfd , EPOLL_CTL_ADD , stdno , &env);
+	while(true)
+	{
+		int ready = epoll_wait(epfd ,(struct epoll_event *)&events , 4 ,-1);
+		for(int i =0 ;i<ready ; i++)
+		{
+			if(events[i].data.fd == stdno)
+			{
+				printf("you: ");
+				scanf("%s",buf);
+				write(clientfd , buf ,255);
+				bzero(buf,255);
+			}
+			else if((events[i].data.fd == clientfd)&&(events[i].events & EPOLLIN))
+			{
+				int tmp = read(clientfd,buf,255);
+				printf("others: %s\n",buf);
+				bzero(buf,255);
+			}
+		}
+	}
+	env.data.fd = clientfd;
+	epoll_ctl(epfd , EPOLL_CTL_DEL , clientfd , &env);
+	env.data.fd = stdno;
+	epoll_ctl(epfd , EPOLL_CTL_DEL , stdno , &env);
+	close(epfd);
 	close(clientfd);
 	return 0;
 }
--- makefile
+++ makefile
@@ -1,12 +1,9 @@
 server:server.o
 	@g++ server.cc -o server
 clean:
-	@rm *.o server client client_resv
+	@rm *.o server client 
 client:client.o
 	@g++ client.cc -o client
-client_resv:client_resv.o
-	@g++ client_resv.cc -o client_resv
 all:server.o client.o
 	@g++ client.cc -o client
 	@g++ server.cc -o server
-	@g++ client_resv.cc -o client_resv
